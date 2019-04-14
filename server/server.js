986c536017f6e860081f56e7774c7039fb90d717
Charlie Winnard <charlie@overtone.co>
2019-04-14 11:31:36 -0600

    database

--- server/server.js
+++ server/server.js
@@ -4,8 +4,14 @@ const fs = require('fs');
 
 const { getDogInfo } = require('../logic/getDogInfo');
 
+const { Dog } = require('./database/models/dog');
+
+
 const app = express();
 const port = 5000;
+
+// Connect to database
+require('./database/connect');
 
 const ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImNkMzBkZmIxODcxYzg1MDdlNGNlMDlhOTE0MWQ1ODA4ZDM2Y2I5NTgwYTdhY2JlNmNiOTI0YmJmNjY1NzdlMTdlOTcxMzkwNGQ4NmZjMWE3In0.eyJhdWQiOiI4bVdlOGhDT0VCWHEzT3RETDhsRTVwdTFTbmhnSWZnMXp0T1Z4MTgwTDRyaDFRVExWcSIsImp0aSI6ImNkMzBkZmIxODcxYzg1MDdlNGNlMDlhOTE0MWQ1ODA4ZDM2Y2I5NTgwYTdhY2JlNmNiOTI0YmJmNjY1NzdlMTdlOTcxMzkwNGQ4NmZjMWE3IiwiaWF0IjoxNTU1MjYxMjM1LCJuYmYiOjE1NTUyNjEyMzUsImV4cCI6MTU1NTI2NDgzNCwic3ViIjoiIiwic2NvcGVzIjpbXX0.qcPRKAQYp2xuTWcwsyIQ1sTpzy7K98d2v1ZJOMAeBPjzv7BHRTPlJGD-DtkIHgjiK3QkONErC5bINDzNupOYY2aRAXbdK8NxEJnaMIbnI9TR8Ff7ugc4i9RYRpyBaoSdWipwdJhw0sxAwOPiMR6UE031UCgkw34ZTuOmU4PYoHe3sdq1diaqKsSZEuI73ywKXUTde_OUh0NmQTwf1tgoJqn68GMi888e2EMeQSYbpIeT1MGTImhuZV5ZzVDXpqNoKmw9We-1iItlDisBhJHtzcBe60YhG9sotVKqPj7r39Q_MKfGJeuF18kqz8CMI5Vicf1TXJCuFH2ggy5TQWLsvg'
 const HEADER =  { headers: { 'Authorization': 'Bearer ' + ACCESS_TOKEN } };
@@ -27,13 +33,12 @@ app.get('/token', (req, res) => {
 
 app.get('/dogs', (req, res) => {
     axios.get('https://api.petfinder.com/v2/animals?organization=CO395', HEADER).then((petfinderRes) => {
-        const json = getDogInfo(petfinderRes.data);
-        fs.writeFile('dogInfo.json', JSON.stringify(json), (e) => {
-            if (e) {
-                console.log(e);
-            }
+        const dogs = getDogInfo(petfinderRes.data);
+        const models = dogs.map((dog) => {
+            return new Dog(dog);
         });
-        res.send(json).status(200);
+        Dog.collection.insertMany(models);
+        res.send(dogs).status(200);
     }, (e) => {
         console.log(e);
         res.send(e).status(500);
