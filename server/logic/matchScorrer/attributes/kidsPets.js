ad5a479777241202fe5dfa66e37186da4524f8d9
Charlie Winnard <charlie@overtone.co>
2019-05-26 12:06:53 -0600

    first green for kids pets

--- server/logic/matchScorrer/attributes/kidsPets.js
+++ server/logic/matchScorrer/attributes/kidsPets.js
@@ -1,4 +1,28 @@
+const consolidateDogPrefs = (dogInfo) => {
+    const dogLoves = [];
+    const dogHates = [];
+
+    if (dogInfo.environment.children === true) {
+        dogLoves.push('children');
+    }
+    if (dogInfo.environment.children === false) {
+        dogHates.push('children');
+    }
+    if (dogInfo.environment.dogs === true) {
+        dogLoves.push('dogs');
+    }
+    if (dogInfo.environment.dogs === false) {
+        dogHates.push('dogs');
+    }
+    return { dogLoves, dogHates };
+}
+
 const getKidsPetsScore = (dogInfo, breedInfo) => {
+    const dogPrefs = consolidateDogPrefs(dogInfo);
+    const { dogLoves, dogHates } = dogPrefs;
+    if (dogLoves.includes('dogs') && dogLoves.includes('children') && breedInfo.goodWith.includes('dogs') && breedInfo.goodWith.includes('children')) {
+        return [6, 6, 6, 2];
+    }
     return [0, 0, 0, 0];
 }
 
