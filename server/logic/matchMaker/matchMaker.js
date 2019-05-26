5881c0002fb4de9ec10aa9d862330c585934f11b
Charlie Winnard <charlie@overtone.co>
2019-05-26 17:54:01 -0600

    matchmaker logic

--- server/logic/matchMaker/matchMaker.js
+++ server/logic/matchMaker/matchMaker.js
@@ -1,8 +1,29 @@
+const _ = require('lodash');
+
+const { Dog } = require('../../database/models/dog');
+
 function MatchMaker() {}
 
-MatchMaker.prototype.getMatchesInOrder = function () {
+const getMatchScore = (dog, quizResponses) => {
+    let score = 0;
+    if (!dog.scoreGrid) {
+        return score;
+    }
+    dog.scoreGrid.keys.forEach((attrScore, index) => {
+        score += attrScore[quizResponses[index]]
+    });
+    console.log(score);
+    return score;
+}
+
+MatchMaker.prototype.getMatchesInOrder = function (quizResponses) {
     return new Promise(function(resolve, reject) {
-        resolve([]);
+        Dog.find({}).then((dogs) => {
+            const sortedMatches = _.sortBy(dogs, (dog) => {
+                return getMatchScore(dog, quizResponses);
+            });
+            resolve(sortedMatches);
+        })
     });
 };
 
