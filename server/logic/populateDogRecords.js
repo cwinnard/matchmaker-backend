b012a2f839696568c261e44647b5653c7f808286
Charlie Winnard <charlie@overtone.co>
2019-05-26 15:52:08 -0600

    typo

--- server/logic/populateDogRecords.js
+++ server/logic/populateDogRecords.js
@@ -1,5 +1,5 @@
 const { Dog } = require('../database/models/dog');
-const MatchScorrer = require('./MatchScorrer/matchScorrer');
+const MatchScorrer = require('./matchScorrer/matchScorrer');
 
 const populateDogRecords = (dogs) => {
     const scorrer = new MatchScorrer();
