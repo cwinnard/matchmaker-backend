3be151968c517dcb9fce2464da22402c19650ad7
Charlie Winnard <charlie@overtone.co>
2019-05-26 13:18:06 -0600

    test refactor

--- test/scorrer.test.js
+++ test/scorrer.test.js
@@ -15,17 +15,14 @@ describe('match scorrer', () => {
         const mediumEnergeticScores = scorrer.getAttributeScore('housing', dogInfo, breedInfo);
         const bigMaxEnergyScores = scorrer.getAttributeScore('housing', { size: 'Large' }, { activityLevel: 'needs lots of activity', characteristics: [] });
 
+        const newBreedInfo = breedInfo;
+        newBreedInfo.characteristics.push('best dogs for apartments dwellers');
+        const apartmentPrefScores = scorrer.getAttributeScore('housing', dogInfo, newBreedInfo);
+
         expect(smallRegularScores).toEqual([2, 2, 6]);
         expect(mediumEnergeticScores).toEqual([6, 6, 2]);
         expect(bigMaxEnergyScores).toEqual([6, 4, 0]);
-        done();
-    });
-
-    it('should give special preference to apartment dog flag housing attr', (done) => {
-        const newBreedInfo = breedInfo;
-        newBreedInfo.characteristics.push('best dogs for apartments dwellers');
-        const housingScores = scorrer.getAttributeScore('housing', dogInfo, newBreedInfo);
-        expect(housingScores).toEqual([1, 1, 6]);
+        expect(apartmentPrefScores).toEqual([1, 1, 6]);
         done();
     });
 
