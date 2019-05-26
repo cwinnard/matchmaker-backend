02c7e5ed9e217517dba86080eb47fe7993d61680
Charlie Winnard <charlie@overtone.co>
2019-05-26 15:43:58 -0600

    update dog model

--- server/database/models/dog.js
+++ server/database/models/dog.js
@@ -9,12 +9,44 @@ var dogSchema = new mongoose.Schema({
         type: String,
         required: false,
     },
+    gender: {
+        type: String,
+        required: false,
+    },
+    breeds: {
+        type: mongoose.Schema.Types.Mixed,
+        required: false,
+    },
+    environment: {
+        type: mongoose.Schema.Types.Mixed,
+        required: false,
+    },
     age: {
         type: String,
         required: false,
     },
     size: {
         type: String,
+        required: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    photos: {
+        type: Array,
+        required: false,
+    },
+    organizationId: {
+        type: Number,
+        required: false,
+    },
+    contact: {
+        type: mongoose.Schema.Types.Mixed,
+        required: false,
+    },
+    scoreGrid: {
+        type: mongoose.Schema.Types.Mixed,
         required: false,
     },
 });
