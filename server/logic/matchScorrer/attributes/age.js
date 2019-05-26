818ca25789e3b44fd0919420fddf15f67b15ef8d
Charlie Winnard <charlie@overtone.co>
2019-05-26 13:25:55 -0600

    age scores green

--- server/logic/matchScorrer/attributes/age.js
+++ server/logic/matchScorrer/attributes/age.js
@@ -1,11 +1,11 @@
 const getAgeScore = (age) => {
     switch(age) {
     case 'Baby':
-        return [0, 0, 0];
+        return [6, 0, 0];
     case 'Adult':
-        return [0, 0, 0];
+        return [0, 6, 2];
     case 'Senior':
-        return [0, 0, 0];
+        return [0, 2, 6];
     default:
         return [0, 0, 0];
     }
