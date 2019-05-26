dc1f804b0f8961606d226924a7e5b3d76437ab97
Charlie Winnard <charlie@overtone.co>
2019-05-26 11:38:28 -0600

    use attribute name + defualt case

--- server/logic/matchScorrer/matchScorrer.js
+++ server/logic/matchScorrer/matchScorrer.js
@@ -3,7 +3,13 @@ const { getHousingScore } = require('./attributes/housing');
 function MatchScorrer() {}
 
 MatchScorrer.prototype.getAttributeScore = function (attributeName, dogInfo, breedInfo) {
-    return getHousingScore(dogInfo, breedInfo);
+    switch(attributeName) {
+    case('housing'):
+        return getHousingScore(dogInfo, breedInfo);
+    default:
+        return [0, 0, 0];
+    }
+
 };
 
 module.exports = MatchScorrer;
--- test/scorrer.test.js
+++ test/scorrer.test.js
@@ -4,6 +4,12 @@ const { dogInfo, breedInfo } = require('./data');
 describe('match scorrer', () => {
     const scorrer = new MatchScorrer();
 
+    it('should give back empty array on default case', (done) => {
+        const expected = scorrer.getAttributeScore('asdfasdf', null, null);
+        expect(expected).toEqual([0, 0, 0]);
+        done();
+    });
+
     it('should determine ideal score for attribute provided', (done) => {
         const smallRegularScores = scorrer.getAttributeScore('housing', { size: 'Small' }, { activityLevel: 'regular exercise', characteristics: [] });
         const mediumEnergeticScores = scorrer.getAttributeScore('housing', dogInfo, breedInfo);
