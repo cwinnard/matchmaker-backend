7050539d462049d066168661ebc0dd9d5ee2e130
Charlie Winnard <charlie@overtone.co>
2019-05-26 11:19:59 -0600

    red test apartment dog

--- server/logic/matchScorrer/attributes/housing.js
+++ server/logic/matchScorrer/attributes/housing.js
@@ -1,5 +1,25 @@
+const activityLevels = {
+    "calm": 1,
+    "regular exercise": 2,
+    "energetic": 3,
+    "needs lots of activity": 4
+}
+
+const sizes = {
+    "small": 1,
+    "medium": 2,
+    "large": 3
+}
+
+const bestApartmentDog = (breedInfo) => {
+    return breedInfo.characteristics.includes('best dogs for apartments dwellers');
+}
+
 const getHousingScore = (dogInfo, breedInfo) => {
     let housingScore = [0, 0, 0];
+    if (bestApartmentDog(breedInfo)) {
+        housingScore = [0, 0, 6];
+    }
     if (breedInfo.activityLevel === 'energetic' && dogInfo.size === 'Medium') {
         housingScore = [6, 4, 2];
     };
--- test/scorrer.test.js
+++ test/scorrer.test.js
@@ -1,5 +1,5 @@
 const MatchScorrer = require('../server/logic/matchScorrer/matchScorrer');
-const { dogInfo, breedInfo } = require('./data');
+let { dogInfo, breedInfo } = require('./data');
 
 describe('match scorrer', () => {
     const scorrer = new MatchScorrer();
@@ -7,6 +7,13 @@ describe('match scorrer', () => {
     it('should determine ideal score for attribute provided', (done) => {
         const housingScores = scorrer.getAttributeScore('housing', dogInfo, breedInfo);
         expect(housingScores).toEqual([6, 4, 2]);
+        done();
+    });
+
+    it('should give special preference to apartment dog flag', (done) => {
+        breedInfo.characteristics.push('best dogs for apartment dwellers');
+        const housingScores = scorrer.getAttributeScore('housing', dogInfo, breedInfo);
+        expect(housingScores).toEqual([0, 0, 6]);
         done();
     });
 });
