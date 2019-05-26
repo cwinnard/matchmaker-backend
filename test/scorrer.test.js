83bf96aa324fd74f6f0ac72b75bf66857e0c6622
Charlie Winnard <charlie@overtone.co>
2019-05-26 11:35:13 -0600

    test all if conditions housing

--- test/scorrer.test.js
+++ test/scorrer.test.js
@@ -5,8 +5,13 @@ describe('match scorrer', () => {
     const scorrer = new MatchScorrer();
 
     it('should determine ideal score for attribute provided', (done) => {
-        const housingScores = scorrer.getAttributeScore('housing', dogInfo, breedInfo);
-        expect(housingScores).toEqual([6, 6, 2]);
+        const smallRegularScores = scorrer.getAttributeScore('housing', { size: 'Small' }, { activityLevel: 'regular exercise', characteristics: [] });
+        const mediumEnergeticScores = scorrer.getAttributeScore('housing', dogInfo, breedInfo);
+        const bigMaxEnergyScores = scorrer.getAttributeScore('housing', { size: 'Large' }, { activityLevel: 'needs lots of activity', characteristics: [] });
+
+        expect(smallRegularScores).toEqual([2, 2, 6]);
+        expect(mediumEnergeticScores).toEqual([6, 6, 2]);
+        expect(bigMaxEnergyScores).toEqual([6, 4, 0]);
         done();
     });
 
