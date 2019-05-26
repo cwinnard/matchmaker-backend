fd783119429ed88b143c548d0297a9b93a70663a
Charlie Winnard <charlie@overtone.co>
2019-05-26 09:55:46 -0600

    eyes

--- server/logic/extractBreedInfo.js
+++ server/logic/extractBreedInfo.js
@@ -40,6 +40,7 @@ const getGoodWith = (infoArray) => {
 
 const extractBreedInfo = (infoArray, akcHandle) => {
     const extractedInfo = {};
+    console.log(akcHandle);
     extractedInfo.name = akcHandle.replace(/-/g, ' ');
     extractedInfo.akcHandle = akcHandle;
     extractedInfo.characteristics = getValue(infoArray, attributeTypes.ARRAY, 'characteristic-');
